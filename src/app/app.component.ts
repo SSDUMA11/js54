56e08419bb575a582461f31aeba7106749ba6213
SSDUMA11 <dumasofia2@gmail.com>
2023-05-06 16:06:32 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-app';
+export class AppComponent  {
+  users = [
+    {name: "Mike", status: "I'm learning angular", url: "https://i.pravatar.cc/150?img=12",},
+    {name: "Nikola", status: "Playing piano", url: "https://i.pravatar.cc/150?img=7",},
+    {name: "Bob", status: "Translations from Chinese", url: "https://i.pravatar.cc/150?img=11", },
+    {name: "Anna ", status: "Pretty girl", url: "https://i.pravatar.cc/150?img=1",},
+];
+
 }
